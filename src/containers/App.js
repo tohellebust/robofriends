280f6b15cd7aae88daae33ab6cd10cc218d8100d
Torgeir Hellebust <torgeir.hellebust@gmail.com>
2023-10-30 21:43:30 +0100

    Revert "updating changes"
    This reverts commit 6bd914c48c699cdcfdd327180c0405322e330d49.
    "Reverting commit 6bd914"

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,37 +6,44 @@ import '../App.css';
 
 
 
-function App() {
-  const [robots, setRobots] = useState([])
-  const [searchfield, setSearchfield] = useState('')
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => {setRobots(users)});
-  }, [count])
-
-  const onSearchChange = (event) => {
-    setSearchfield(event.target.value)   
+class App extends Component {
+  constructor() {
+    super()
+    this.state= {
+      robots: [],
+      searchfield: ''
+    }
   }
 
-  const filteredRobots = robots.filter(robot => {
-    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-  })
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {this.setState({ robots: users})});
+  }
 
-  return (!robots.length) ?
-    <h1>Loading..</h1> :
-    (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <button onClick={() => setCount(count + 1)}>Click Me!</button>
-        <SearchBox searchChange={onSearchChange}/>
-        <Scroll>
-          <CardList robots={filteredRobots}/>
-        </Scroll>
-      </div>
-    );      
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })   
+  }
+
+  render () {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+
+    return (!robots.length) ?
+      <h1>Loading..</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots}/>
+          </Scroll>
+        </div>
+      );      
+  }
 }
 
 export default App;
